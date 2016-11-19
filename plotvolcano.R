dd355772c01025152f295adf9244145ae72a5948
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-11-19 16:12:25 +0000

    An R script to plot a volcano

---
+++ plotvolcano.R
@@ -0,0 +1,1 @@
+plot(x=rep(1:61, 87)+rnorm(61*87,sd=0.15), y=t(volcano)+rnorm(61*87,sd=0.15), col=c(rep("red",61), rep("blue",61)), ylab="Height in metres", main="Profile of a volcano", ylim=range(volcano), pch=3, bty="n", xlab="East/West coordinates [10m]")
